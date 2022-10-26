d990c693ab4866416f4c0bc98af1803375c63c3f
TeremotoBettoni <mtbettoni@gmail.com>
2022-10-26 14:39:39 -0300

    Clase y actividad prctica de metodos con doWhile y Scanner

--- src/cl/generation/f20221026/Actividad.java
+++ src/cl/generation/f20221026/Actividad.java
@@ -5,23 +5,28 @@ import java.util.Scanner;
 public class Actividad {
 
 	public static void main(String[] args) {
-		// 
+		//
 		String fullName = getNombreCompleto();
-		Float IMC = calculoIMC();
 		int edadPersona = edad();
+		Float IMC = calculoIMC();
 		
-		System.out.println("El nombre del paciente es: "+fullName+", de edad: "+edadPersona+", la cual tiene un IMC de: "+IMC);
-
+		System.out.println("El nombre del paciente es: " + fullName+". De edad: " + edadPersona+". Tiene un IMC de: " + IMC);
 	}
+
+	
 	public static String getNombreCompleto() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Ingrese su nombre");
-		String nombreCompleto = sc.next();
-		System.out.println("Tu nombre es: "+nombreCompleto);
-		sc.close();
+		System.out.println("ingresa tu nombre");
+		String nombre= sc.next();
+		System.out.println("ingresa tu apellido paterno");
+		String apePaterno= sc.next();
+		System.out.println("ingresa tu apellido materno");
+		String apeMaterno= sc.next();
+		String nombreCompleto = nombre+" "+apePaterno+" "+apeMaterno;
+		//sc.close();
 		return nombreCompleto;
 	}
-	
+
 	public static int edad() {
 		Scanner sc = new Scanner(System.in);
 		int edad = 0;
@@ -32,54 +37,42 @@ public class Actividad {
 
 		} while (edad < 18);
 
-		System.out.println("Edad ingresada " + edad);
-		
-		sc.close();
+		//sc.close();
 		return edad;
 	}
-	
+
 	public static Float calculoIMC() {
-		// Capturara datos ingresados por el usuario
-		Scanner sc = new Scanner(System.in);// declaracion de variable tipo Scanner nombreDeVariable = new
-											// Scanner(System.in)
-		// calculo de IMC; imc= kilos/(estatura*estatura);
-		// en el caso de los kilos,. son datos tipo float
+		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Ingrese su pero en kilogramos");
 
-		Float peso = sc.nextFloat();// aqui le pedimos al usuario ingresar los datos, dependiendo del sistema colocando el sc que declare en variable scanne + el .nextFloat()
-									// operativo ingresamos los numeros con . o ,
-		System.out.println("El peso del paciente es: " + peso);
+		Float kilo = sc.nextFloat();
+		System.out.println("El kilo del paciente es: " + kilo);
 
 		System.out.println("Ingrese la altura en metros");
-		Float altura = sc.nextFloat();// aqui le pedimos al usuario ingresar los datos de altura
+		Float altura = sc.nextFloat();
 		System.out.println("La altura del paciente es: " + altura);
 
-		Float IMC = peso / (altura * altura);
-		
-		nivelIMC(IMC);// y aqui llamamos a la funcion o metodo para que nos arroje el nivel en el que estamos
+		Float IMC = kilo / (altura * altura);
 
-		sc.close(); // aqui cierro la variable
+		nivelIMC(IMC);
+
+		//sc.close(); 
 		return IMC;
 	}
+
 	public static void nivelIMC(Float IMC) {
-		/*
-		 * Por debajo de 18.5 Bajo peso 
-		 * 18.5 - 24.9 Normal 
-		 * 25.0 - 29.9 Sobrepeso 
-		 * 30.0 o más Obeso
-		 */
-		
-		if(IMC<18.5) {
-			System.out.println("El paciente esta bajo de peso");
-		}else if (IMC>=18.5 && IMC<25) {// le colocamos 25 para que tome todo los decimales desde 24.999999 y menores
+
+		if (IMC < 18.5) {
+			System.out.println("El paciente esta bajo de kilo");
+		} else if (IMC >= 18.5 && IMC < 25) {// le colocamos 25 para que tome todo los decimales desde 24.999999 y
+												// menores
 			System.out.println("El paciente esta en el rango normal");
-		}else if (IMC>=25 && IMC<30) {
-			System.out.println("El paciente esta con sobrepeso");
-		}else {
+		} else if (IMC >= 25 && IMC < 30) {
+			System.out.println("El paciente esta con sobrekilo");
+		} else {
 			System.out.println("El paciente esta con obecidad");
 		}
-		
 
 	}
 
